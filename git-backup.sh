380978d17b3520d8190c3f516dfcbd83a0bb3a0e
Patrick Pokatilo <mail@shyxormz.net>
2017-07-28 02:25:20 +0200

    Add wrapper that backs up unsaved work in Git repositories

---
+++ git-backup.sh
@@ -0,0 +1,62 @@
+git_backup() {
+  local CURRENT_DIR="$(pwd)"
+
+  echo "\033[0;32mBacking up unsaved work\033[0m"
+
+  git_backup_repository "."
+
+  if [ ! -f composer.json ]; then
+    if [ "$(git rev-parse --show-toplevel 2> /dev/null)" != "" ]; then
+      cd "$(git rev-parse --show-toplevel)"
+    fi
+  fi
+
+  if [ -f composer.json ]; then
+    for dependency in $($(php_composer) show | awk '{print $1}'); do
+      if [ -d "${PWD}/vendor/${dependency}/.git" ]; then
+        git_backup_repository "${PWD}/vendor/${dependency}"
+      fi
+    done
+  fi
+}
+
+git_backup_repository() {
+  cd "$1"
+
+  if [ "$(git rev-parse --show-toplevel 2> /dev/null)" = "" ]; then
+    # not a Git repository
+    return
+  fi
+
+  if [ "$(git rev-parse --all)" = "" ]; then
+    # Git repository does not contain any refs
+    return
+  fi
+
+  if [ "$(git status --porcelain)" = "" ] && \
+     [ "$(git log --branches --not --remotes)" = "" ] && \
+     [ "$(git stash list)" = "" ]; then
+    # Git repository does not contain any changes
+    return
+  fi
+
+  local REPONAME="$(git rev-parse --show-toplevel | xargs basename)"
+
+  echo "  - Backing up Git repository to \033[32m$(git_backup_target "${REPONAME}")\033[0m (\033[0;33m$(echo $1 | sed 's@.*/vendor/@@')\033[0m)"
+
+  git bundle create "$(git_backup_target "${REPONAME}")" --all 2> /dev/null
+}
+
+git_backup_target() {
+  echo "${HOME}/Desktop/${1}.git"
+}
+
+case "$1" in
+  install|update)
+    git_backup
+    ;;
+
+  *)
+    # safe operation, do nothing
+    ;;
+esac
